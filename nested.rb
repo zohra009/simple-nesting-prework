969a1da650be30ebc0a685f918ee3e4a025b8770
anzohra@outlook.com <anzohra@outlook.com>
2018-06-22 12:09:20 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -123,5 +123,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-  programmer_hash[:dennis_ritchie][:languages] >> ""
+  programmer_hash[:dennis_ritchie][:languages] >> "Assembly"
 end
